9930cdad6becf67b2e8cb0c6963b6f766ecd5875
Marcus Bass <Bass.Marcus@icloud.com>
2019-03-20 10:44:20 -0700

    Deploy prep

---
+++ public/build/precache-manifest.65f04480b763c5a226cd0a5b9ceed0fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "cbce2eaf21952a40fd7b",
+    "url": "/static/js/main.86c5a8ea.chunk.js"
+  },
+  {
+    "revision": "e4addb25354477b478ca",
+    "url": "/static/js/2.04486d2f.chunk.js"
+  },
+  {
+    "revision": "cbce2eaf21952a40fd7b",
+    "url": "/static/css/main.0dae50a8.chunk.css"
+  },
+  {
+    "revision": "f93bd8bc2bf75903c6e887602f35be09",
+    "url": "/index.html"
+  }
+];
