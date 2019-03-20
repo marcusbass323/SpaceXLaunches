9e98e8d5c0bc3e650ea973dcc6e894b36c2a64f1
Marcus Bass <Bass.Marcus@icloud.com>
2019-03-19 20:33:17 -0700

    Completed

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Launches from './components/Launches';
+import Launch from './components/Launch';
+import './App.css';
+import logo from './logo.png'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
+
+class App extends Component {
+  render() {
+    return (
+      <div className="SC">
+      <ApolloProvider client={client}>     
+      <Router>
+      <div className="container">
+        <img alt="" src={logo}
+        style={{
+          width: 750,
+          display: 'block',
+          margin: 'auto'
+        }}
+        />
+        <Route exact path="/" component={Launches} />
+        <Route exact path="/launch/:flight_number" component={Launch} />
+      </div>
+      </Router>
+      </ApolloProvider>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/MissionKey.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function MissionKey() {
+  return (
+    <div className="my-3">
+      <h4>
+        <span className="px-3 mr-2 bg-success" /> = Success
+      </h4>
+      <h4>
+        <span className="px-3 mr-2 bg-danger" /> = Fail
+      </h4>
+    </div>
+  )
+}
